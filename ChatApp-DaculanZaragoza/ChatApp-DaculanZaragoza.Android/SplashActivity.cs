cb73c3870d725fbcf904ebabb8a5423283a425dc
Eque Daculan <Eque Daculan@LAPTOP-F6L239KO>
2022-04-22 14:17:01 +0800

    Add project files.

---
+++ ChatApp-DaculanZaragoza/ChatApp-DaculanZaragoza.Android/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Android;
+
+namespace ChatApp_DaculanZaragoza.Droid
+{
+    [Activity(Label = "ChatApp", Icon = "@mipmap/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+    //public class SplashActivity : ContentPage
+    //{
+    //    public SplashActivity()
+    //    {
+    //        Content = new StackLayout
+    //        {
+    //            Children = {
+    //                new Label { Text = "Welcome to Xamarin.Forms!" }
+    //            }
+    //        };
+    //    }
+    //}
+}
